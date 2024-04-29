ec02f47dd6fc52ea2d621e31b3d8964e5bb00b40
Bianchi <bianchi@jhob.com.br>
2024-04-29 14:56:40 -0300

    install filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\TenancyServiceProvider::class
+    App\Providers\Filament\InitPanelProvider::class,
+    App\Providers\TenancyServiceProvider::class,
 ];
--- routes/tenant.php
+++ routes/tenant.php
@@ -24,8 +24,8 @@ Route::middleware([
     InitializeTenancyByDomain::class,
     PreventAccessFromCentralDomains::class,
 ])->group(function () {
-    Route::get('/', function () {
-        dd(User::all());
-        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
-    });
+//    Route::get('/', function () {
+//        dd(User::all());
+//        return 'This is your multi-tenant application. The id of the current tenant is ' . tenant('id');
+//    });
 });
