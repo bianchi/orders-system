92af24b4f8ed6ba903cd2486be3ec3628c5d6cc2
Bianchi <bianchi@jhob.com.br>
2024-04-29 18:03:33 -0300

    add users resource

--- config/database.php
+++ config/database.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Str;
 return [
     'default'     => env('DB_CONNECTION', 'sqlite'),
     'connections' => [
-        'pgsql' => [
+        'pgsql'   => [
             'driver'         => 'pgsql',
             'url'            => env('DB_URL'),
             'host'           => env('DB_HOST', '127.0.0.1'),
@@ -17,6 +17,20 @@ return [
             'prefix'         => '',
             'prefix_indexes' => true,
             'search_path'    => 'public',
+            'sslmode'        => 'prefer',
+        ],
+        'tenant1' => [
+            'driver'         => 'pgsql',
+            'url'            => env('DB_URL'),
+            'host'           => env('DB_HOST', '127.0.0.1'),
+            'port'           => env('DB_PORT', '5432'),
+            'database'       => env('DB_DATABASE', 'laravel'),
+            'username'       => env('DB_USERNAME', 'root'),
+            'password'       => env('DB_PASSWORD', ''),
+            'charset'        => env('DB_CHARSET', 'utf8'),
+            'prefix'         => '',
+            'prefix_indexes' => true,
+            'search_path'    => 'tenantempresa1',
             'sslmode'        => 'prefer',
         ],
     ],
