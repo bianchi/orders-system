88429141f83f50dad58a67efeba8c644dde6a622
Bianchi <bianchi@jhob.com.br>
2024-04-30 14:18:32 -0300

    test list records

--- tests/Feature/Filament/Resources/UserResource/Pages/ListUsersTest.php
+++ tests/Feature/Filament/Resources/UserResource/Pages/ListUsersTest.php
@@ -5,9 +5,21 @@ declare(strict_types=1);
 namespace Tests\Filament\Resources\UserResource\Pages;
 
 use App\Filament\Resources\UserResource;
+use App\Models\User;
+
+use function Pest\Livewire\livewire;
 
 it('can render page', function () {
-    //    $this->withoutExceptionHandling();
     $this->get(UserResource::getUrl('index'));
     $this->get(UserResource::getUrl('index'))->assertSuccessful();
+});
+
+it('can list records', function () {
+    $users = User::factory()->count(9)->create();
+    $users->add($this->user);
+
+    livewire(UserResource\Pages\ListUsers::class)
+        ->assertCanSeeTableRecords($users)
+        ->assertCanRenderTableColumn('name')
+        ->assertCanRenderTableColumn('email');
 });
