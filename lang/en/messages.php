4824eef16bd7db737b32827e2f13f702b3c12da7
Bianchi <bianchi@jhob.com.br>
2024-05-01 12:54:26 -0300

    portuguese translation

---
+++ lang/en/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'cannot_delete_account_owner' => 'The account owner cannot be deleted.',
+    'error'                       => 'Erros',
+];
---
+++ lang/pt_BR/messages.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'cannot_delete_account_owner' => 'O proprietário da conta não pode ser excluído.',
+    'error'                       => 'Erros',
+];
