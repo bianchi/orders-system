3bd49a68dd7848abf43cf85f9baa455a59f648f3
Bianchi <bianchi@jhob.com.br>
2024-05-05 09:48:32 -0300

    configures the date format

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,9 @@ namespace App\Providers;
 use App\Enums\Role;
 use App\Policies\PermissionPolicy;
 use App\Policies\RolePolicy;
+use Filament\Forms\Components\DateTimePicker;
 use Filament\Tables\Columns\Column;
+use Filament\Tables\Table;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,5 +32,10 @@ class AppServiceProvider extends ServiceProvider
 
         Gate::policy(\Spatie\Permission\Models\Permission::class, PermissionPolicy::class);
         Gate::policy(\Spatie\Permission\Models\Role::class, RolePolicy::class);
+
+        DateTimePicker::configureUsing(static fn () => DateTimePicker::$defaultDateDisplayFormat = __('format.date'));
+        DateTimePicker::configureUsing(static fn () => DateTimePicker::$defaultDateTimeDisplayFormat = __('format.dateTime'));
+        Table::configureUsing(static fn () => Table::$defaultDateDisplayFormat = __('format.date'));
+        Table::configureUsing(static fn () => Table::$defaultDateTimeDisplayFormat = __('format.dateTime'));
     }
 }
--- todo.md
+++
@@ -1,3 +0,0 @@
-Permitir exclusão de funções, exceto a de super admin
-Redirecionar para o index após cadastro/alteração de funções e permissões. Se aceitarem meu PR
-Esconder o guard. Se aceitarem meu PR
